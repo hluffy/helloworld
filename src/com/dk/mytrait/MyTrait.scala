75d40cbb4c21c4b82486eca49d6f565f834c4433
hluffy <18801613171@163.com>
2017-07-03 17:42:01 +0800

    接口

---
+++ src/com/dk/mytrait/MyTrait.scala
@@ -0,0 +1,23 @@
+package com.dk.mytrait
+
+object MyTrait {
+  trait Equal{
+    def isEqual(x:Any):Boolean;
+    def isNotEqual(x:Any):Boolean = !isEqual(x);
+  }
+  
+  class Point(xc:Int,yc:Int) extends Equal{
+    var x:Int = xc;
+    var y:Int = yc;
+    def isEqual(obj:Any) = obj.isInstanceOf[Point]&&obj.asInstanceOf[Point].x == x;
+  }
+  
+  def main(args:Array[String]):Unit={
+    var p1 = new Point(2,3);
+    var p2 = new Point(2,4);
+    var p3 = new Point(3,4);
+    println(p1.isEqual(p2));
+    println(p1.isEqual(p3));
+    println(p1.isEqual(2));
+  }
+}
