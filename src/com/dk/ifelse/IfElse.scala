006557fcdf83d4aeb31f065661b7d54698e03829
hluffy <18801613171@163.com>
2017-07-03 13:22:34 +0800

    if else和循环

---
+++ src/com/dk/ifelse/IfElse.scala
@@ -0,0 +1,18 @@
+package com.dk.ifelse
+
+object IfElse {
+  def main(args: Array[String]):Unit={
+    //if else语句
+    var x = 20;
+//    if(x<20){
+//      println(x+"<20");
+//    }else{
+//      println(x+">=20");
+//    }
+    
+      //循环语句
+    while(true){
+      println(x);
+    }
+  }
+}
